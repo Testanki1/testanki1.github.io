38d4fa40bc9306f1a3034d859df13acac277bbc3
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2023-01-06 16:38:48 +0800

    Delete api.php?action=webapp-manifest directory

--- api.php?action=webapp-manifest/index.php
+++
@@ -1,1 +0,0 @@
-{"name":"Tanki Online Wiki","orientation":"portrait","dir":"ltr","lang":"en","display":"browser","theme_color":"#eaecf0","background_color":"#fff","start_url":"/Tanki_Online_Wiki","icons":[]}
