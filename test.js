035aab0224f92f9ea4fa90e36c49477e35e3b299
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2024-01-19 21:38:47 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@
       "expireDate": "2024-01-20T02:00:00Z",
       "resources": "https://public-deploy6.test-eu.tankionline.com/resources/",
       "switchVersion": false
-    }
+    },
     "default": {
       "balancer": "https://balancer.eu.tankionline.com/balancer-android",
       "config": "https://c%server%.eu.tankionline.com/config.xml",
