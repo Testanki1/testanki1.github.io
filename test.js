a5ce5f9b60d7f8297a4e2f72dc5f1b673f77e4c9
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2024-01-19 21:25:39 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 {
   "datacenters": {
-    "default": {deploy4
+    "1693397564": {
+      "balancer": "https://balancer.public-deploy6.test-eu.tankionline.com/balancer-android",
+      "config": "https://c%server%.public-deploy6.test-eu.tankionline.com/config.xml",
+      "expireDate": "2024-01-20T02:00:00Z",
+      "resources": "https://public-deploy6.test-eu.tankionline.com/resources/",
+      "switchVersion": false
+    }
+    "default": {
       "balancer": "https://balancer.eu.tankionline.com/balancer-android",
       "config": "https://c%server%.eu.tankionline.com/config.xml",
       "expireDate": "2024-01-20T13:01:13.382Z",
