a7760ecb81dcf8a1ae45643bf7981e8cad00d0dc
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2024-01-19 21:13:04 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+{
+  "datacenters": {
+    "default": {deploy4
+      "balancer": "https://balancer.eu.tankionline.com/balancer-android",
+      "config": "https://c%server%.eu.tankionline.com/config.xml",
+      "expireDate": "2024-01-20T13:01:13.382Z",
+      "resources": "https://s.eu.tankionline.com/resources/",
+      "switchVersion": false
+    }
+  },
+  "versions": { }
+}
