17397f9a2318b9b64b759124969695d27dd98841
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2024-10-19 00:57:52 +0800

    Create script.user.js

---
+++ models/script.user.js
@@ -0,0 +1,474 @@
+// ==UserScript==
+// @name         3D坦克皮肤模型替换
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  替换3D坦克炮塔、底盘、无人机皮肤模型
+// @author       Testanki
+// @match        *://*.3dtank.com/play*
+// @match        *://*.tankionline.com/play*
+// @match        *://*.test-eu.tankionline.com/*
+// @match        *://*.test-ru.tankionline.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+        const currentVersionCode = 1; // 当前脚本版本
+    const versionUrl = 'https://testanki1.github.io/models/version.json'; // 版本检查的 JSON 文件地址
+
+    // 检查更新
+    fetch(versionUrl)
+        .then(response => {
+            if (!response.ok) throw new Error('网络错误');
+            return response.json();
+        })
+        .then(data => {
+            const latestVersion = data.version; // 获取最新版本号
+            const latestVersionCode = data.version_code; // 获取最新版本代码
+            const updateInfo = data.update_info; // 获取更新内容
+            const downloadUrl = data.download_url; // 获取下载链接
+
+            // 比较版本代码
+            if (latestVersionCode > currentVersionCode) {
+                const message = `
+                    有新版本可用！\n
+                    最新版本：${latestVersion}\n
+                    更新内容：${updateInfo}\n
+                `;
+                if (confirm(message + "点击确定下载新版本？")) {
+                    window.location.href = downloadUrl; // 确定则下载
+                }
+            }
+        })
+        .catch(error => {
+            console.error('更新检查失败:', error);
+        });
+    
+const currentUrl = window.location.href;
+    // 设定镭射炮、极速炮默认值
+    let shaftDefault;
+    if (/^https:\/\/3dtank\.com\/play\//.test(currentUrl)) {
+        // 如果是国服网址
+        shaftDefault = "0/114/160/315/31033605025210";
+    } else {
+        // 其他网址
+        shaftDefault = "622/21305/321/374/31105044674676"; // 默认 HD
+    }
+    let vulcanDefault;
+    if (/^https:\/\/3dtank\.com\/play\//.test(currentUrl)) {
+        // 如果是国服网址
+        vulcanDefault = "0/16722/20/40/31033604715057";
+    } else {
+        // 其他网址
+        vulcanDefault = "622/107753/242/303/31122501347624"; // 默认 HD
+    }
+	const turretsRedirectMap = {
+		"firebird": {
+			"default": "573/113511/153/137/31033604622310",
+			"XT": "0/16722/167/100/31033604530020",
+			"LC": "606/154706/226/46/31033604523453",
+			"DC": "574/111547/344/362/31033604472221",
+			"DC_OLD": "546/140033/371/67/31033604465511",
+			"GT": "620/113215/220/43/31033604507506"
+		},
+		"freeze": {
+			"default": "575/153310/123/250/31033604537313",
+			"XT": "545/126533/221/204/31033604562720",
+			"XT_HD": "607/133452/43/130/31033604565055",
+			"LC": "605/14613/143/127/31033604552551",
+			"IC": "605/135171/211/104/31033604546324",
+			"GT": "613/146472/243/156/31033604531607",
+			"SE": "575/141301/263/323/31033604560325"
+		},
+		"isida": {
+			"default": "605/12650/334/263/31033604666004",
+			"XT": "547/121262/134/345/31033604677132",
+			"LC": "606/155040/263/253/31033604672363",
+			"GT": "605/12655/270/246/31033604660301"
+		},
+		"tesla": {
+			"default": "567/20040/100/57/31033605122122",
+			"XT_HD": "567/20040/100/30/31033605140327",
+			"LC": "604/60235/244/25/31033605125726",
+			"RF": "616/167677/151/223/31033605130425"
+		},
+		"hammer": {
+			"default": "611/147301/37/333/31033604645545",
+			"XT": "550/160307/363/221/31033604643362",
+			"LC": "601/166273/204/221/31033604634650",
+			"DC": "604/4215/36/135/31033605266363"
+		},
+		"twins": {
+			"default": "575/4122/336/247/31033605203717",
+			"XT": "547/35525/56/66/31033605232035",
+			"LC": "577/157371/340/255/31033605210405",
+			"SP": "573/113554/112/100/31033605224225",
+			"RF": "607/24114/77/214/31033605215045",
+			"GT": "617/163502/325/41/31033605175257"
+		},
+		"ricochet": {
+			"default": "603/121326/210/264/31033604765662",
+			"XT": "546/5477/152/352/31033605004772",
+			"LC": "556/131232/204/234/31033604772501",
+			"RF": "577/176465/41/10/31033605000151"
+		},
+		"vulcan": {
+			"default": vulcanDefault,
+            "HD": "622/107753/242/303/31122501347624",
+			"XT": "0/16722/260/334/31033604733427",
+			"PR": "556/15741/256/125/31033605241104",
+			"UT": "560/31363/210/347/31033604717360",
+			"DC": "613/2044/314/224/31033604703141"
+		},
+		"smoky": {
+			"default": "566/114246/64/4/31033605044215",
+			"XT": "0/114/153/53/31033605053755",
+			"LC": "577/171773/42/54/31033605047550",
+			"GT": "607/133661/133/27/31033605036471"
+		},
+		"striker": {
+			"default": "0/16723/37/11/31033605013174",
+			"XT": "551/70756/233/273/31033605117137",
+			"UT": "570/164502/316/245/31033605234224"
+		},
+		"thunder": {
+			"default": "601/105644/16/124/31033604626332",
+			"XT": "0/16722/167/101/31033605170145",
+			"PR": "557/14251/175/251/31033605155427",
+			"UT": "551/122165/142/136/31033605157155",
+			"LC": "545/14356/174/306/31033605151121",
+			"GT": "603/104171/41/115/31033605030161",
+			"XT_HD": "617/134472/113/60/31033605164111"
+		},
+		"scorpion": {
+			"default": "600/40107/4/364/31033605243165",
+			"XT_HD": "602/132677/206/41/31033605253521"
+		},
+		"magnum": {
+			"default": "0/16723/57/323/31033604453663",
+			"XT": "550/75104/53/350/31033604732253",
+			"SP": "612/42416/374/133/31033604725533"
+		},
+		"railgun": {
+			"default": "567/105205/202/122/31033604741475",
+			"XT": "0/16722/6/301/31033604764033",
+			"LC": "550/121443/145/146/31033604745456",
+			"PR": "553/116715/27/132/31033604752652",
+			"UT": "556/177362/212/346/31033604754562",
+			"GT": "606/155010/245/142/31033604735353"
+		},
+		"gauss": {
+			"default": "611/61722/256/76/31033604653206",
+			"XT": "560/124462/246/14/31033604617041",
+			"PR": "554/41313/45/141/31033604572567",
+			"UT": "563/51105/72/133/31033605272237",
+			"GT": "613/146442/233/316/31033604574743",
+			"IC": "614/75662/326/41/31033604602505"
+		},
+		"shaft": {
+			"default": shaftDefault,
+            "HD": "622/21305/321/374/31105044674676",
+			"XT": "546/73531/62/216/31033605014624",
+			"LC": "600/170471/174/26/31033605260624"
+		}
+	};
+
+	const hullsRedirectMap = {
+		"wasp": {
+			"default": "574/111243/33/322/31033607311775",
+			"XT": "0/16722/167/77/31033610130736",
+			"DC": "574/113351/211/154/31033610052500",
+			"LC": "577/171773/42/62/31033610115062",
+			"GT": "620/112773/325/5/31033610100325"
+		},
+		"hopper": {
+			"default": "564/5207/367/304/31033610326736",
+			"XT_HD": "564/41402/173/47/31033610315703",
+			"RF": "616/167677/151/211/31033607331063"
+		},
+		"hornet": {
+			"default": "566/70102/323/346/31033607367072",
+			"XT": "0/16722/6/305/31033607424605",
+			"LC": "551/32007/310/225/31033607400400",
+			"PR": "553/1466/317/276/31033607413764",
+			"UT": "562/165115/303/236/31033610210055",
+			"GT": "605/27506/77/216/31033607347661"
+		},
+		"viking": {
+			"default": "571/121215/5/23/31033607765702",
+			"XT": "0/16722/167/76/31033610034165",
+			"LC": "545/14403/373/22/31033607776373",
+			"PR": "557/14273/215/344/31033610007323",
+			"UT": "552/54655/57/366/31033610356100",
+			"GT": "603/64520/263/244/31033607745375",
+			"XT_HD": "606/155145/337/205/31033610020277",
+			"DC": "604/7224/253/317/31033610256112"
+		},
+		"crusader": {
+			"default": "566/4547/232/306/31033607177563",
+			"XT_HD": "566/40410/335/237/31033610341433",
+			"RF": "607/24114/77/31/31033607224317"
+		},
+		"hunter": {
+			"default": "567/166366/55/140/31033610271513",
+			"XT": "547/121236/44/244/31033607523721",
+			"LC": "577/157453/256/241/31033607506717",
+			"PR": "554/155720/136/73/31033607521775",
+			"UT": "561/113562/137/140/31033610302174",
+			"GT": "607/133630/253/171/31033607471473",
+			"DC": "613/14407/324/50/31033607446007"
+		},
+		"paladin": {
+			"default": "573/47363/125/65/31033610154443",
+			"XT_HD": "573/47363/125/60/31033610367705",
+			"RF": "577/176465/41/12/31033607640405"
+		},
+		"dictator": {
+			"default": "602/61700/245/106/31033607256604",
+			"XT": "546/125503/267/14/31033607303502",
+			"LC": "600/170471/174/17/31033610146055",
+			"GT": "606/154745/265/375/31033607235725",
+			"SP": "621/133615/104/57/31066757323353"
+		},
+		"titan": {
+			"default": "606/22645/10/357/31033607664142",
+			"XT": "545/41207/304/132/31033607734572",
+			"LC": "601/166273/204/222/31033607677022",
+			"PR": "555/103037/265/247/31033607711541",
+			"SP": "612/40333/350/361/31033607720032"
+		},
+		"ares": {
+			"default": "560/117661/334/334/31033607160324",
+			"XT_HD": "562/161162/24/375/31033610137021"
+		},
+		"mammoth": {
+			"default": "600/67314/131/54/31033607623773",
+			"XT": "0/16722/260/335/31033607615546",
+			"LC": "557/31354/323/254/31033607553520",
+			"UT": "571/76747/372/131/31033610235472",
+			"SP": "573/113554/112/106/31033607575155",
+			"GT": "617/163502/325/33/31033607536216"
+		}
+	};
+
+	const dronesRedirectMap = {
+		"hyperion": {
+			"default": "556/107004/326/35/30545000710771",
+			"XT": "603/140170/104/322/30545000710642"
+		},
+		"crisis": {
+			"default": "562/45273/110/127/30545000710447",
+			"XT": "602/142250/300/167/30545000710756"
+		}
+	};
+
+
+// 允许的选项正则表达式
+	const hullsPattern = /^(XT|XT_HD|LC|PR|UT|DC|GT|RF|SP)$/i;
+	const turretsPattern = /^(HD|XT|XT_HD|LC|PR|UT|DC|DC_OLD|IC|GT|RF|SE|SP)$/i;
+	const dronesPattern = /^(XT)$/i;
+
+	// 从 localStorage 中获取上次的选择
+	let lastHullsChoice = localStorage.getItem('userChoiceHulls') || 'XT';
+	let lastTurretChoice = localStorage.getItem('userChoiceTurret') || 'XT';
+	let lastDroneChoice = localStorage.getItem('userChoiceDrone') || 'XT';
+
+	// 选择底盘，确保用户输入合法
+	let userChoiceHulls = prompt("请选择要使用的底盘模型替换 (XT/XT_HD(XT 高清)/LC（遗产）/PR（青春）/UT（超高）/DC（恶魔）/GT（跑车）/RF（复古未来）/SP（蒸汽朋克）):", lastHullsChoice);
+	if (userChoiceHulls === null) {
+		userChoiceHulls = ''; // 用户点击取消，设置为空
+	} else {
+		while (!hullsPattern.test(userChoiceHulls)) {
+			alert("输入无效，请输入有效的底盘皮肤系列：XT, XT_HD, LC, PR, UT, DC, GT, RF, SP");
+			userChoiceHulls = prompt("请选择要使用的底盘模型替换 (XT/XT_HD(XT 高清)/LC（遗产）/PR（青春）/UT（超高）/DC（恶魔）/GT（跑车）/RF（复古未来）/SP（蒸汽朋克）):", lastHullsChoice);
+			if (userChoiceHulls === null) {
+				userChoiceHulls = ''; // 用户点击取消，设置为空
+				break;
+			}
+		}
+	}
+
+	// 选择炮塔，确保用户输入合法
+	let userChoiceTurret = prompt("请选择要使用的炮塔模型替换 (HD(高清)/XT/XT_HD（XT 高清）/LC（遗产）/PR（青春）/UT（超高）/DC（恶魔）/DC_OLD（恶魔旧）/IC（冰）/GT（跑车）/RF（复古未来）/SE（秘密）/SP（蒸汽朋克）):", lastTurretChoice);
+	if (userChoiceTurret === null) {
+		userChoiceTurret = ''; // 用户点击取消，设置为空
+	} else {
+		while (!turretsPattern.test(userChoiceTurret)) {
+			alert("输入无效，请输入有效的炮塔皮肤系列：HD, XT, XT_HD, LC, PR, UT, DC, DC_OLD, IC, GT, RF, SE, SP");
+			userChoiceTurret = prompt("请选择要使用的炮塔模型替换 (HD（高清）/XT/XT_HD（XT 高清）/LC（遗产）/PR（青春）/UT（超高）/DC（恶魔）/DC_OLD（恶魔旧）/IC（冰）/GT（跑车）/RF（复古未来）/SE（秘密）/SP（蒸汽朋克）):", lastTurretChoice);
+			if (userChoiceTurret === null) {
+				userChoiceTurret = ''; // 用户点击取消，设置为空
+				break;
+			}
+		}
+	}
+
+	// 选择无人机，确保用户输入合法
+	let userChoiceDrone = prompt("请选择要使用的无人机模型替换 (XT):", lastDroneChoice);
+	if (userChoiceDrone === null) {
+		userChoiceDrone = ''; // 用户点击取消，设置为空
+	} else {
+		while (!dronesPattern.test(userChoiceDrone)) {
+			alert("输入无效，请输入有效的无人机皮肤系列：XT");
+			userChoiceDrone = prompt("请选择要使用的无人机模型替换 (XT):", lastDroneChoice);
+			if (userChoiceDrone === null) {
+				userChoiceDrone = ''; // 用户点击取消，设置为空
+				break;
+			}
+		}
+	}
+
+	// 确认继续
+	const confirmationMessage = `您选择了底盘 ${userChoiceHulls ? userChoiceHulls.toUpperCase() : '未选择'}、炮塔 ${userChoiceTurret ? userChoiceTurret.toUpperCase() : '未选择'} 和无人机 ${userChoiceDrone ? userChoiceDrone.toUpperCase() : '未选择'}。点击确定继续。`;
+	if (confirm(confirmationMessage)) {
+		// 将用户选择存储到 localStorage
+		localStorage.setItem('userChoiceHulls', userChoiceHulls);
+		localStorage.setItem('userChoiceTurret', userChoiceTurret);
+		localStorage.setItem('userChoiceDrone', userChoiceDrone);
+        
+		if (userChoiceHulls && hullsRedirectMap[userChoiceHulls]) {
+			const hullsMap = hullsRedirectMap;
+			document.querySelectorAll('script, link, img, audio, video, source').forEach(tag => {
+				for (const key in hullsMap) {
+					if (tag.src && tag.src.includes(hullsMap[key].default)) {
+						const newSrc = hullsMap[key][userChoiceHulls];
+						if (newSrc) {
+							tag.src = tag.src.replace(hullsMap[key].default, newSrc);
+							// Check if replacing 'dictator' to 'SP' and replace URL accordingly
+							if (key === "dictator" && userChoiceHulls.toUpperCase() === "SP") {
+								tag.src = tag.src.replace("res.3dtank.com", "s.eu.tankionline.com");
+							}
+						}
+					}
+					if (tag.href && tag.href.includes(hullsMap[key].default)) {
+						const newHref = hullsMap[key][userChoiceHulls];
+						if (newHref) {
+							tag.href = tag.href.replace(hullsMap[key].default, newHref);
+							// Check if replacing 'dictator' to 'SP' and replace URL accordingly
+							if (key === "dictator" && userChoiceHulls.toUpperCase() === "SP") {
+								tag.href = tag.href.replace("res.3dtank.com", "s.eu.tankionline.com");
+							}
+						}
+					}
+				}
+			});
+		}
+
+
+		// 替换炮塔资源
+		if (userChoiceTurret && turretsRedirectMap[userChoiceTurret]) {
+			const turretMap = turretsRedirectMap;
+			document.querySelectorAll('script, link, img, audio, video, source').forEach(tag => {
+				for (const key in turretMap) {
+					if (tag.src && tag.src.includes(turretMap[key].default)) {
+						const newSrc = turretMap[key][userChoiceTurret];
+						if (newSrc) {
+							tag.src = tag.src.replace(turretMap[key].default, newSrc);
+                            if ((key === "shaft" || key === "vulcan") && userChoiceTurret.toUpperCase() === "HD") {
+								tag.src = tag.src.replace("res.3dtank.com", "s.eu.tankionline.com");
+							}
+						}
+					}
+					if (tag.href && tag.href.includes(turretMap[key].default)) {
+						const newHref = turretMap[key][userChoiceTurret];
+						if (newHref) {
+							tag.href = tag.href.replace(turretMap[key].default, newHref);
+                            if ((key === "shaft" || key === "vulcan") && userChoiceTurret.toUpperCase() === "HD") {
+								tag.href = tag.href.replace("res.3dtank.com", "s.eu.tankionline.com");
+							}
+						}
+					}
+				}
+			});
+		}
+
+		// 替换无人机资源
+		if (userChoiceDrone && dronesRedirectMap[userChoiceDrone]) {
+			const droneMap = dronesRedirectMap;
+			document.querySelectorAll('script, link, img, audio, video, source').forEach(tag => {
+				for (const key in droneMap) {
+					if (tag.src && tag.src.includes(droneMap[key].default)) {
+						const newSrc = droneMap[key][userChoiceDrone];
+						if (newSrc) {
+							tag.src = tag.src.replace(droneMap[key].default, newSrc);
+						}
+					}
+					if (tag.href && tag.href.includes(droneMap[key].default)) {
+						const newHref = droneMap[key][userChoiceDrone];
+						if (newHref) {
+							tag.href = tag.href.replace(droneMap[key].default, newHref);
+						}
+					}
+				}
+			});
+		}
+
+		// 拦截 fetch 请求
+		const originalFetch = window.fetch;
+		window.fetch = function(input, init) {
+			if (typeof input === 'string') {
+				for (const key in hullsRedirectMap) {
+					if (input.includes(hullsRedirectMap[key].default) && hullsRedirectMap[key][userChoiceHulls.toUpperCase()]) {
+						input = input.replace(hullsRedirectMap[key].default, hullsRedirectMap[key][userChoiceHulls.toUpperCase()]);
+						// Check if replacing 'dictator' to 'SP' and replace URL accordingly
+						if (key === "dictator" && userChoiceHulls.toUpperCase() === "SP") {
+							input = input.replace("res.3dtank.com", "s.eu.tankionline.com");
+						}
+						break;
+					}
+				}
+				for (const key in turretsRedirectMap) {
+					if (input.includes(turretsRedirectMap[key].default) && turretsRedirectMap[key][userChoiceTurret.toUpperCase()]) {
+						input = input.replace(turretsRedirectMap[key].default, turretsRedirectMap[key][userChoiceTurret.toUpperCase()]);
+                        if ((key === "shaft" || key === "vulcan") && userChoiceTurret.toUpperCase() === "HD") {
+							input = input.replace("res.3dtank.com", "s.eu.tankionline.com");
+						}
+						break;
+					}
+				}
+				for (const key in dronesRedirectMap) {
+					if (input.includes(dronesRedirectMap[key].default) && dronesRedirectMap[key][userChoiceDrone.toUpperCase()]) {
+						input = input.replace(dronesRedirectMap[key].default, dronesRedirectMap[key][userChoiceDrone.toUpperCase()]);
+						break;
+					}
+				}
+			}
+			return originalFetch(input, init);
+		};
+
+		// 拦截 XMLHttpRequest 请求
+		const originalOpen = XMLHttpRequest.prototype.open;
+		XMLHttpRequest.prototype.open = function(method, url) {
+			for (const key in hullsRedirectMap) {
+				if (url.includes(hullsRedirectMap[key].default) && hullsRedirectMap[key][userChoiceHulls.toUpperCase()]) {
+					url = url.replace(hullsRedirectMap[key].default, hullsRedirectMap[key][userChoiceHulls.toUpperCase()]);
+					// Check if replacing 'dictator' to 'SP' and replace URL accordingly
+					if (key === "dictator" && userChoiceHulls.toUpperCase() === "SP") {
+						url = url.replace("res.3dtank.com", "s.eu.tankionline.com");
+					}
+					break;
+				}
+			}
+			for (const key in turretsRedirectMap) {
+				if (url.includes(turretsRedirectMap[key].default) && turretsRedirectMap[key][userChoiceTurret.toUpperCase()]) {
+					url = url.replace(turretsRedirectMap[key].default, turretsRedirectMap[key][userChoiceTurret.toUpperCase()]);
+                    if ((key === "shaft" || key === "vulcan") && userChoiceTurret.toUpperCase() === "HD") {
+						url = url.replace("res.3dtank.com", "s.eu.tankionline.com");
+					}
+					break;
+				}
+			}
+			for (const key in dronesRedirectMap) {
+				if (url.includes(dronesRedirectMap[key].default) && dronesRedirectMap[key][userChoiceDrone.toUpperCase()]) {
+					url = url.replace(dronesRedirectMap[key].default, dronesRedirectMap[key][userChoiceDrone.toUpperCase()]);
+					break;
+				}
+			}
+			// Similar checks for turretsRedirectMap and dronesRedirectMap...
+			originalOpen.call(this, method, url);
+		};
+
+	} else {
+		alert("选择已取消，页面将继续加载原始内容。");
+	}
+})();
