c8575ffea8521966b019357db939278157223736
Testanki1 <104706055+Testanki1@users.noreply.github.com>
2025-05-01 19:16:44 +0800

    Update translations.js

--- translations.js
+++ translations.js
@@ -766,6 +766,7 @@ const replacementDict = {
 "Rapid-fire mode": "快速射击模式",
 "Healing Emitters": "治疗发射器",
 "Quasar": "类星体",
+"Excelsior": "精进者",
 "Heat Resistance": "耐热性",
 "Cold Resistance": "耐寒性",
 "Lightweight Construction": "轻型结构",
