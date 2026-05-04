6cbc7e3260fa922480d1cee839ed78963d411fbe
Testanki <104706055+Testanki1@users.noreply.github.com>
2026-05-04 10:17:55 +0800

    Update translations.js

--- translations.js
+++ translations.js
@@ -281,7 +281,6 @@ const replacementDict = {
 "Commander": "统帅",
 "Generalissimo": "大元帅",
 "Legend": "传奇",
-"general information": "一般信息",
 "map": "地图",
 "Aleksandrovsk": "亚历山大罗夫斯克",
 "Archipelago": "群岛",
